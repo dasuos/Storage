2b64291c8a2cb7f4f4ffc4064a046d25e0f1fd9a
Daniel Krnak <krnakd@pa-stepankoval.vlp.cz>
2018-02-15 15:19:19 +0100

    FakePath corrected

--- Core/FakePath.php
+++ Core/FakePath.php
@@ -4,7 +4,16 @@ namespace Dasuos\Storage;
 
 final class FakePath implements Path {
 
+    private $directory;
+
+    public function __construct(string $directory = '') {
+        $this->directory = $directory;
+    }
+
 	public function location(string $path): string {
 		return $path;
 	}
+    public function directory(): string {
+        return $this->directory;
+    }
 }
